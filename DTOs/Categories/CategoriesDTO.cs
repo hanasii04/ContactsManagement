3180b2e014a6eaea52e0e3de2bf4085f2223dd24
hanasii04 <leonah0004@gmail.com>
2025-12-29 22:19:32 +0700

    feat: implement category management feature for users

---
+++ DTOs/Categories/CategoriesDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ContactsManagement.DTOs.Categories
+{
+	public class CategoriesDTO
+	{
+		public int CategoriesId { get; set; }
+		public int UserId { get; set; }
+		[Required(ErrorMessage = "Vui lòng nhập tên danh mục")]
+		public string Name { get; set; }
+		public DateTime CreatedAt { get; set; }
+		public DateTime? UpdatedAt { get; set; }
+	}
+}
